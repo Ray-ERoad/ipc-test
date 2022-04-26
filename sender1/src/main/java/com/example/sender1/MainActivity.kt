195527141b8e7a9901cf9b61f886aab3e38c048d
Ray Qu <ray.qu@eroad.co>
2022-04-27 09:44:53 +1200

    Keep both StartService and SendBroadcast method.

--- sender1/src/main/java/com/example/sender1/MainActivity.kt
+++ sender1/src/main/java/com/example/sender1/MainActivity.kt
@@ -16,6 +16,13 @@ class MainActivity : AppCompatActivity() {
             while(true) {
                 for (i in 0..200) {
                     count++
+
+//                    val componentName = ComponentName("com.example.testipc", "com.example.testipc.IntentReceiverService")
+//                    val intent = Intent()
+//                    intent.component = componentName
+//                    intent.putExtra("Data", "App1: $count")
+//                    this.startForegroundService(intent)
+
                     Intent().also { intent ->
                         intent.component = ComponentName("com.example.testipc", "com.example.testipc.IntentBroadcastReceiver")
                         intent.setAction("com.test.intent.action.DEFAULT")
