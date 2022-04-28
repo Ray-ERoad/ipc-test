0b0fa5072b781bc17397f18b2440ad0bf65efc29
Ray Qu <ray.qu@eroad.co>
2022-04-28 13:51:10 +1200

    Call Service from BroadcastReceiver

--- app/src/main/java/com/example/testipc/IntentBroadcastReceiver.kt
+++ app/src/main/java/com/example/testipc/IntentBroadcastReceiver.kt
@@ -1,6 +1,7 @@
 package com.example.testipc
 
 import android.content.BroadcastReceiver
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.util.Log
@@ -8,5 +9,16 @@ import android.util.Log
 class IntentBroadcastReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         Log.d("IntentBroadcastReceiver", "intent: ${intent?.extras?.get("Data")}")
+        if (context == null || intent == null) return
+
+        startService(context, intent)
+    }
+
+    private fun startService(context: Context, intent: Intent) {
+        val componentName = ComponentName("com.example.testipc", "com.example.testipc.IntentBroadcastReceiver")
+        val newIntent = Intent()
+        newIntent.component = componentName
+        newIntent.putExtra("Data", intent.getStringExtra("Data"))
+        context.startForegroundService(intent)
     }
 }
--- sender1/src/main/java/com/example/sender1/MainActivity.kt
+++ sender1/src/main/java/com/example/sender1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.sender1
 
+import android.app.PendingIntent
 import android.content.ComponentName
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -16,23 +17,29 @@ class MainActivity : AppCompatActivity() {
             while(true) {
                 for (i in 0..200) {
                     count++
-
-//                    val componentName = ComponentName("com.example.testipc", "com.example.testipc.IntentReceiverService")
-//                    val intent = Intent()
-//                    intent.component = componentName
-//                    intent.putExtra("Data", "App1: $count")
-//                    this.startForegroundService(intent)
-
-                    Intent().also { intent ->
-                        intent.component = ComponentName("com.example.testipc", "com.example.testipc.IntentBroadcastReceiver")
-                        intent.setAction("com.test.intent.action.DEFAULT")
-                        intent.putExtra("Data", "App1: $count")
-                        sendBroadcast(intent)
-                    }
+//                    startService()
+                    sendBroadcast()
                 }
 
                 Thread.sleep(1000)
             }
         }.start()
+    }
+
+    private fun startService() {
+        val componentName = ComponentName("com.example.testipc", "com.example.testipc.IntentBroadcastReceiver")
+        val intent = Intent()
+        intent.component = componentName
+        intent.putExtra("Data", "App1: $count")
+        this.startForegroundService(intent)
+    }
+
+    private fun sendBroadcast() {
+        Intent().also { intent ->
+            intent.component = ComponentName("com.example.testipc", "com.example.testipc.IntentBroadcastReceiver")
+            intent.setAction("com.test.intent.action.DEFAULT")
+            intent.putExtra("Data", "App1: $count")
+            sendBroadcast(intent)
+        }
     }
 }
